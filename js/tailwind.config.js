994b3c255d459e944e33c42ba647c32db0623896
Mikel Edwin <michealanthonyxxx@gmail.com>
2023-07-28 13:51:32 +0100

    Day 12

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,9 +1,12 @@
-tailwind.config = {
-    theme: {
-        extend: {
-            screens: {
-                'sm': {'min': '0px', 'max':'1023px'}
+try{
+    tailwind.config = {
+        theme: {
+            extend: {
+                screens: {
+                    'sm': {'min': '0px', 'max':'1023px'},
+                    'xl': {'min': '0px'}
+                }
             }
         }
     }
-}
+}catch{}
